7e6d171ebd5fc9a56b6e2716839e6f0fa199c420
swesley400 <telhado.folha@gmail.com>
2024-10-11 22:00:44 -0300

    feat: KAN89 configurando exemplos de uso no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import './styles/app.css';
 import EditorWithTables from 'lib/template/generateHeader';
 import PDFDocumentBuilder from 'lib/printer/PDFDocumentBuilder';
+import { ImageUploadComponent } from 'components/ImageUpload';
+import { ClinicaImage } from 'interface/clinicaImage.interface';
+import { useState } from 'react';
 
 function App() {
+
+  const clinicaImages: ClinicaImage[]  = [{
+    imageName: "Logo",
+    imageBase64: "",
+    imageId: 1
+  }];
+
+
   return (
-    <>  
-      <EditorWithTables></EditorWithTables>
+    <> 
+      {/*Exemplos de uso de nossos components*/ } 
+      <EditorWithTables clinicImages={clinicaImages} />      
       <PDFDocumentBuilder />
+      <ImageUploadComponent onSave={() => {}}/>
     </>
   );
-}
+}   
 
 export default App;
