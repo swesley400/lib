43ba290aef9a6ebf8cb05f10c461523d72eaa245
swesley400 <telhado.folha@gmail.com>
2024-11-16 00:16:25 -0300

    kan-92

--- src/components/ExamData.tsx
+++ src/components/ExamData.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const ExamData: React.FC<IExamDataProps> = ({ fields }) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', fontSize: '12px' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '10px', fontSize: '12px', }}>
       <div style={{ flex: 1 }}>
         {fields.slice(0, Math.ceil(fields.length / 2)).map((field, index) => (
           <div key={index}>
